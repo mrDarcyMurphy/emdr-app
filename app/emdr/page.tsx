60b0b2ae701d08f62bd091c3c15f7ad61e59dd64
Mr. Darcy Murphy <darcy.murphy@loudbit.com>
2023-05-06 14:33:52 -0500

    recreated old emdr app, plus initial chatgpt version

--- app/emdr/page.tsx
+++ app/emdr/page.tsx
@@ -1,21 +1,94 @@
 'use client'
 
-import { gsap } from 'gsap'
-import { useLayoutEffect, useRef } from 'react'
+import { gsap, Sine } from 'gsap'
+import { useLayoutEffect, useEffect, useRef, useState } from 'react'
+import { Howl } from 'howler';
+
+import './page.css'
+
 
 export default function Page() {
-  const emdr = useRef()
-  const circle = useRef()
+  const [isAnimating, setIsAnimating] = useState<boolean>(false);
+  const [leftSound, setLeftSound] = useState<Howl | null>(null);
+  const [rightSound, setRightSound] = useState<Howl | null>(null);
+  const lightRef = useRef<HTMLDivElement>(null);
 
-  useLayoutEffect(() => {
-    let ctx = gsap.context(() => {
-      // gsap.
-    })
-  }, [])
+  const toggleAnimation = (): void => {
+    setIsAnimating(!isAnimating);
+    if (!isAnimating) {
+      leftSound?.pause();
+      rightSound?.pause();
+    }
+  };
+
+  useEffect(() => {
+    setLeftSound(new Howl({ src: ["om-left.wav"] }));
+    setRightSound(new Howl({ src: ["om-right.wav"] }));
+  }, []);
+
+  useEffect(() => {
+    const light = lightRef.current;
+
+    let tl: gsap.core.Timeline | undefined;
+    // if (isAnimating) {
+    //   tl = gsap.timeline({
+    //     repeat: -1,
+    //     yoyo: true,
+    //     onUpdate: () => {
+    //       const lightPos = gsap.getProperty(light, "x");
+    //       console.debug(`🔊 lightPos:`, typeof lightPos, lightPos)
+    //       if (lightPos === 95) {
+    //         rightSound?.play();
+    //       } else if (lightPos === "0vw") {
+    //         leftSound?.play();
+    //       }
+    //     },
+    //   });
+    //   tl.to(light, { x: "95vw", duration: 1, ease: "power2.inOut" });
+    // }
+    // if (isAnimating) {
+    //   tl = gsap.timeline({
+    //     repeat: -1,
+    //     yoyo: true,
+    //     onUpdate: () => {
+    //       const lightPos = gsap.getProperty(light, "left");
+    //       if (lightPos === 100) {
+    //         rightSound?.play();
+    //       } else if (lightPos === 0) {
+    //         leftSound?.play();
+    //       }
+    //     },
+    //   });
+    //   tl.to(light, { left: "100%", duration: 1, ease: "power1.inOut" });
+    // }
+    if (isAnimating) {
+
+      tl = gsap.timeline({repeat: -1});
+      tl
+        .to('.light', 1, { left: '100%', ease: Sine.easeInOut })
+        .call(function() { rightSound?.play() }, [], "-=0.145924")
+        .to('.light', 1, { left: '0%', ease: Sine.easeInOut })
+        .call(function() { leftSound?.play() }, [], "-=0.145924")
+
+    }
+
+    return () => {
+      if (tl) {
+        tl.kill();
+      }
+    };
+  }, [isAnimating, leftSound, rightSound]);
+
 
   return (
-    <div className="emdr" ref={emdr}>
-      <div className="circle p-4 bg-blue max-w-xs border border-lime-600" ref={circle}>x</div>
-    </div>
+    <>
+      <button onClick={toggleAnimation}>
+        {isAnimating ? "Stop Animation" : "Start Animation"}
+      </button>
+      <div className="light-box">
+        <div className="light" ref={lightRef} />
+      </div>
+    </>
   )
+
 }
