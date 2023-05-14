ee2cb4b42d9989a07d1ada308d5a177de56584ba
Mr. Darcy Murphy <darcy.murphy@loudbit.com>
2023-05-13 20:15:35 -0500

    fix ts issue

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,4 +1,9 @@
-export function Button({ onClick, children }) {
+interface ButtonProps {
+  onClick: () => void;
+  children: React.ReactNode;
+}
+
+export function Button({ onClick, children }: ButtonProps) {
   return (
     <button className="bg-slate-800 text-slate-400 hover:text-slate-300 rounded-sm py-1 px-2" onClick={onClick}>{children}</button>
   )
